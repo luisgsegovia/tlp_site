e1afe51ae7c01c96f2e04324fbe301818428f6af
Rob <rochovian@gmail.com>
2018-12-21 02:17:16 -0500

    still need to require an answer for each step, fix the alignment of skills section, and make mobile work

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,7 +1,7 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
 var dict = {
-  1: "Technology Engineering Program",
-  2: "Technology Leadership Program",
+  1: "The Technology Engineering Program",
+  2: "The Technology Leadership Program",
   3: "STEP 1",
   4: "STEP 2"
 }
@@ -26,7 +26,8 @@ function showTab(n) {
   var decision = 0;
   if (n == (x.length - 1)) {
     decision = calculateDecision();
-    if(decision == 5){
+    if (decision == 5) {
+      
       console.log("Couldn't find match");
     }
   }
@@ -37,20 +38,33 @@ function showTab(n) {
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else if (n == (x.length - 1)) {
-    document.getElementById("prevBtn").innerHTML = '<a href="' + link[decision] + '">' + 'Learn More About ' + dict[decision] + '</a>';
-    document.getElementById("prevBtn").style.display = "inline";
-    document.getElementById("stepCounter").style.display = "none";
+    //display nothing if the logic couldnt find anything
+    if (decision == 5) {
+      document.getElementById("prevBtn").style.display = "none";
+      $("#finalMessage").html("Hmm, looks like we couldn't find a match. Click below to read more about all our programs.")
+    } else {
+      document.getElementById("prevBtn").innerHTML = 'Learn more about ' + dict[decision];
+      document.getElementById("prevBtn").style.display = "inline";
+      document.getElementById("stepCounter").style.display = "none";
+      $("#regForm").css("margin", "auto");
+      $("#regForm").css("padding", "0");
+    }
   } else {
     document.getElementById("prevBtn").style.display = "inline";
   }
 
   if (n == (x.length - 1)) {
     document.getElementById("nextBtn").innerHTML = "View all our programs";
+    $("#programLink").attr("href", link[decision]);
+    $("nextBtn").attr("onclick", "");
+
     x = document.getElementsByClassName("breakline");
     // A loop that checks every input field in the current tab:
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "block";
     }
+    setTimeout(setLinks, 1000);
+
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
@@ -60,12 +74,17 @@ function showTab(n) {
   // Card Display
 }
 
+function setLinks() {
+  $("#splashLink").attr("href", "../Splash_Page/splash.html");
+}
+
 function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab");
   // console.log("NextPrev Parameter: " + n);
   // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
+  // if (n == 1 && !validateForm()) return false;
+  // if(!validateForm()) return;
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
@@ -77,31 +96,37 @@ function nextPrev(n) {
     return false;
   }
   // Otherwise, display the correct tab:
-  // console.log("Number of tabs: " + x.length);
-  // console.log("Current tab: " + currentTab)
 
   showTab(currentTab);
 }
 
 function validateForm() {
   // This function deals with validation of the form fields
-  var x, y, i, valid = true, validCounter = 0;
+  var x, y, i, valid = false, validCounter = 0;
   x = document.getElementsByClassName("tab");
   y = x[currentTab].getElementsByTagName("input");
   // A loop that checks every input field in the current tab:
-  // for (i = 0; i < y.length; i++) {
-  //     if(y[i].type = "input"){
-  //       if(y[i].checked){
-  //         validCounter++;
-  //       }
-  //     }
-  // }
-  // console.log(y);
+  for (i = 0; i < y.length; i++) {
+      if(y[i].type = "input"){
+        if(y[i].checked){
+          valid=true;
+        }
+      }
+  }
+
+  if(y.length == 0 ){
+    valid = true;
+  }
+  console.log(valid);
   //   If the valid status is true, mark the step as finished and valid:
-  if (true) {
+  if (valid) {
     document.getElementsByClassName("step")[currentTab].className += " finish";
+    $("#nextBtn").removeAttr('disabled');
+    $("#nextBtn").css("background-color","#056dae");
   } else {
     valid = false;
+    $("#nextBtn").prop("disabled","true");
+    $("#nextBtn").css("background-color","gray");
   }
   // console.log(valid)
   return valid; // return the valid status
@@ -115,8 +140,6 @@ function fixStepIndicator(n) {
   }
   //... and adds the "active" class to the current step:
   x[n].className += " active";
-  // console.log("step = " + n);
-  // console.log("active step = " + x[n].className)
 }
 
 // 1 = TEP, 2 = TLP, 3 = Step 1, 4 = Step 2
@@ -125,22 +148,24 @@ function calculateDecision() {
   x = document.getElementsByClassName("results-card");
   // console.log(x);
   //step 1
-  if ($("#year1").prop("checked", true) || $("#year2").prop("checked", true)) {
+  if ($("#year1").prop("checked") || $("#year2").prop("checked")) {
     x[2].style.display = "block";
     $("#decision").html(dict[3]);
     decision = 3;
+    // console.log("Year 1: " + $("#year1").prop("checked", true));
+    // console.log("Year 2: " + $("#year2").prop("checked", true));
     //step 2
-  } else if ($("#year3").prop("checked", true)) {
+  } else if ($("#year3").prop("checked")) {
     x[3].style.display = "block";
     $("#decision").html(dict[4]);
     decision = 4;
     //tlp
-  } else if ($("#future2").prop("checked", true) || $("#future4").prop("checked", true)){
+  } else if ($("#future2").prop("checked") || $("#future4").prop("checked")) {
     x[1].style.display = "block";
     $("#decision").html(dict[2]);
     decision = 2;
     //tep
-  } else if  ($("#future2").prop("checked", true) || $("#future4").prop("checked", true)){
+  } else if ($("#future1").prop("checked") || $("#future3").prop("checked")) {
     x[0].style.display = "block";
     $("#decision").html(dict[1]);
     decision = 1;
