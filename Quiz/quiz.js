a951a58387856ccf859b82821ad0b830642cc769
RSSMalnati <stevenmalnati@gmail.com>
2018-12-28 05:45:37 -0500

    fixed some css issues with quiz
    changed font sizes/display style for smaller screens. fixed footer issue where it wasn't working properly.

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -208,3 +208,29 @@ function getRadioVal(form, name) {
   }
   return val; // return value of checked radio or undefined if none checked
 }
+
+
+let menuIcon = document.querySelector('.menuIcon');
+let nav = document.querySelector('.overlay-menu');
+
+menuIcon.addEventListener('click', () => {
+    if (nav.style.transform != 'translateX(0%)') {
+        nav.style.transform = 'translateX(0%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    } else { 
+        nav.style.transform = 'translateX(-100%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    }
+});
+
+
+// Toggle Menu Icon ========================================
+let toggleIcon = document.querySelector('.menuIcon');
+
+toggleIcon.addEventListener('click', () => {
+    if (toggleIcon.className != 'menuIcon toggle') {
+        toggleIcon.className += ' toggle';
+    } else {
+        toggleIcon.className = 'menuIcon';
+    }
+});
