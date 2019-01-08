5cdaa6bacc4ad46843e3626e7f565dc01093bef8
Abigail Collins <abigail.collins@citi.com>
2019-01-08 13:48:46 -0500

    Fix footer mobile responsiveness for splash page

--- Splash_Page/splash.js
+++ Splash_Page/splash.js
@@ -1,8 +1,35 @@
-function myFunction() {
-    var x = document.getElementById("disappear");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+var mobileFooterHide = function(id){
+    $(document).ready(function(){
+        $("#"+id).click(function(){
+            var mq = window.matchMedia( "(min-width: 650px)" );
+                 if (mq.matches) {
+                    return;
+                }
+                if($("ul#"+id+" div").hasClass('disappear-phone')) {
+                    $("ul#"+id+" div").removeClass("disappear-phone");
+                    $("ul#"+id+" i").removeClass("fa-chevron-down");
+                    $("ul#"+id+" i").addClass("fa-chevron-up");
+                } else {
+                    $("ul#"+id+" div").addClass("disappear-phone");
+                    $("ul#"+id+" i").removeClass("fa-chevron-up");
+                    $("ul#"+id+" i").addClass("fa-chevron-down");
+                }                 
+        });
+    });
+}
+
+
+mobileFooterHide("citi-consumer-business");
+
+mobileFooterHide("citi-institutional-business");
+
+mobileFooterHide("citi-media");
+
+mobileFooterHide("citi-relations");
+
+mobileFooterHide("citi-about");
+
+mobileFooterHide("citi-progress");
+
+ 
+
