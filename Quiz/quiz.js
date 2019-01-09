1b83815f201a676de6d05826cb9c30e1fd65feb4
Abigail Collins <abigail.collins@citi.com>
2019-01-09 10:39:26 -0500

    Fix footer mobile responsiveness for quiz page - styling still needs work

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -244,3 +244,39 @@ function getRadioVal(form, name) {
 //         toggleIcon.className = 'menuIcon';
 //     }
 // });
+
+
+var mobileFooterHide = function(id){
+  $(document).ready(function(){
+      $("#"+id).click(function(){
+          var mq = window.matchMedia( "(min-width: 650px)" );
+               if (mq.matches) {
+                  return;
+              }
+              if($("ul#"+id+" div").hasClass('disappear-phone')) {
+                  $("ul#"+id+" div").removeClass("disappear-phone");
+                  $("ul#"+id+" i").removeClass("fa-chevron-down");
+                  $("ul#"+id+" i").addClass("fa-chevron-up");
+              } else {
+                  $("ul#"+id+" div").addClass("disappear-phone");
+                  $("ul#"+id+" i").removeClass("fa-chevron-up");
+                  $("ul#"+id+" i").addClass("fa-chevron-down");
+              }                 
+      });
+  });
+}
+
+
+mobileFooterHide("citi-consumer-business");
+
+mobileFooterHide("citi-institutional-business");
+
+mobileFooterHide("citi-media");
+
+mobileFooterHide("citi-relations");
+
+mobileFooterHide("citi-about");
+
+mobileFooterHide("citi-progress");
+
+
